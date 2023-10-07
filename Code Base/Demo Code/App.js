c3efff1555e4d3c7a8c916b22396900d58f394ba
riturajrajan <prita.rituraj@gmail.com>
2023-10-07 20:44:56 +0530

    React basic

--- Code Base/Demo Code/App.js
+++ Code Base/Demo Code/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { ReactDOM } from "react";
-
 const heading = React.createElement('h1', {}, 'Welcome to react course');
+const heading2 = React.createElement('h2', {}, 'Welcome to react course');
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('root1'));
 root.render(heading);
+root2.render(heading2);
